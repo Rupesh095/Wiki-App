8a4dfc9873bdc3157ba43ead12e28d16263036f6
Rupesh Sahu <rupeshksahu7@gmail.com>
2020-12-10 13:25:06 +0530

    modify app repository class

--- app/src/main/java/com/app/wikiapp/repository/AppRepository.kt
+++ app/src/main/java/com/app/wikiapp/repository/AppRepository.kt
@@ -1,4 +1,36 @@
 package com.app.wikiapp.repository
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.app.wikiapp.data.AppModel
+import com.app.wikiapp.network.RetrofitClient
+import com.google.gson.Gson
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 class AppRepository {
+
+    var articles : MutableLiveData<AppModel>
+
+    init {
+        articles = MutableLiveData()
+    }
+
+    fun getArticle(): MutableLiveData<AppModel> {
+
+        RetrofitClient.service.getArticle().enqueue(object : Callback<AppModel> {
+
+            override fun onFailure(call: Call<AppModel>, t: Throwable) {
+
+            }
+
+            override fun onResponse(call: Call<AppModel>, response: Response<AppModel>) {
+                articles.value = response.body()
+                Log.d("GETARTICLE", Gson().toJson(articles))
+            }
+        })
+        return articles
+    }
+
 }
