65993906c96f259decce2c0e7cd1810768ad5941
Rupesh Sahu <rupeshksahu7@gmail.com>
2020-12-10 12:38:14 +0530

    init commit

---
+++ app/src/main/java/com/app/wikiapp/repository/AppRepository.kt
@@ -0,0 +1,4 @@
+package com.app.wikiapp.repository
+
+class AppRepository {
+}
---
+++ app/src/main/java/com/app/wikiapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.app.wikiapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.wikiapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
